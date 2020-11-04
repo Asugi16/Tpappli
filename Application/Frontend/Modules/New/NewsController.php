d25eba8957d3ccc66f135959cb7c5bdbc3aa54c8
Asugi16 <66683757+Asugi16@users.noreply.github.com>
2020-11-04 16:33:20 +0100

    2

--- Application/Frontend/Modules/New/NewsController.php
+++ Application/Frontend/Modules/New/NewsController.php
@@ -3,36 +3,23 @@ namespace App\Frontend\Modules\News;
 
 use \OCFram\BackController;
 use \OCFram\HTTPRequest;
+use \Entity\Comment;
 
 class NewsController extends BackController
 {
-  public function executeIndex(HTTPRequest $request)
+  // ...
+  
+  public function executeShow(HTTPRequest $request)
   {
-    $nombreNews = $this->app->config()->get('nombre_news');
-    $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
+    $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
     
-    // On ajoute une définition pour le titre.
-    $this->page->addVar('title', 'Liste des '.$nombreNews.' dernières news');
-    
-    // On récupère le manager des news.
-    $manager = $this->managers->getManagerOf('News');
-    
-    // Cette ligne, vous ne pouviez pas la deviner sachant qu'on n'a pas encore touché au modèle.
-    // Contentez-vous donc d'écrire cette instruction, nous implémenterons la méthode ensuite.
-    $listeNews = $manager->getList(0, $nombreNews);
-    
-    foreach ($listeNews as $news)
+    if (empty($news))
     {
-      if (strlen($news->contenu()) > $nombreCaracteres)
-      {
-        $debut = substr($news->contenu(), 0, $nombreCaracteres);
-        $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
-        
-        $news->setContenu($debut);
-      }
+      $this->app->httpResponse()->redirect404();
     }
     
-    // On ajoute la variable $listeNews à la vue.
-    $this->page->addVar('listeNews', $listeNews);
+    $this->page->addVar('title', $news->titre());
+    $this->page->addVar('news', $news);
+    $this->page->addVar('comments', $this->managers->getManagerOf('Comments')->getListOf($news->id()));
   }
 }
